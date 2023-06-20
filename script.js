ef451e60cbeebed35935266d887e055c84fe2676
c-Whitesell <connor.whitesell02@gmail.com>
2023-06-20 09:17:13 -0700

    Create a grid that is dynamically changed using slider
    This commit will replace a lot of existing code in both the styles and
    script document. I came to the conclusion that rather then fussing with
    flexbox why not use grid to create a grid. So that is what this commit
    achieves by replacing code in both the styles and script files a grid is
    created that is dynamically changeable by the user.

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const grid = document.querySelector('.grid-wrapper');
-const slider = document.querySelector('.slider');
+let slider = document.querySelector('.slider');
 const value = document.querySelector('.slider-value');
-value.textContent = slider.value;
-console.log(parseInt(slider.value));
-const flexBasis = 100 / parseInt(slider.value);
-console.log(flexBasis);
 
 const stylesheet = document.styleSheets[0];
-console.log(typeof stylesheet);
-const squareRule = [...stylesheet.cssRules].find(
-  (r) => r.selectorText === '.square'
+const gridRule = [...stylesheet.cssRules].find(
+  (r) => r.selectorText === '.grid-wrapper'
 );
-
+// sets initial val of text on page to starting val of range slider
+value.textContent = slider.value;
+// creates a grid based off the starting value of the range slider (16x16)
+gridRule.style.setProperty(
+  'grid-template-columns',
+  `repeat(${slider.value} , 1fr)`
+);
+// updates the value shown to the user upon changing range slider
 slider.addEventListener('change', function () {
-  const slider = document.querySelector('.slider');
+  slider = document.querySelector('.slider');
   value.textContent = slider.value;
+  grid.textContent = '';
+  gridCreator(parseInt(slider.value));
+  gridRule.style.setProperty(
+    'grid-template-columns',
+    `repeat(${slider.value} , 1fr)`
+  );
 });
-
-function gridCreator(gridNum, flexBasis) {
+// creates the number of div's needed for the grid.
+function gridCreator(gridNum) {
   i = 0;
   while (i < gridNum * gridNum) {
     let square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
-    squareRule.style.setProperty('flex-basis', flexBasis + '%');
-    console.log(flexBasis);
+    console.log(1);
     i++;
   }
 }
 
-gridCreator(parseInt(slider.value), flexBasis);
+gridCreator(parseInt(slider.value));
